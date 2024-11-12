ab3dd8b2f59fb2fe5db8ae832160d522f3a9f8c9
YSB-RB <yousub.ali@roybrand.com>
2024-11-12 22:01:32 +0530

    modified pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 export { Button } from "./components/Button"
 export { Alert } from "./components/Alert"
-
-
-// export { Footer } from "./components/Footer"
-// export { Sidebar } from "./components/Sidebar"
-// export { Navbar } from "./components/Navabr"
-
+export { Footer } from "./components/Footer"
+export { Sidebar } from "./components/Sidebar"
+export { Navbar } from "./components/Navabr"
 
 
 export { HomePage } from "./pages/home"
 export { AboutPage } from "./pages/about"
 export { ContactPage } from "./pages/contact"
+export { ServicePage } from "./pages/service"
