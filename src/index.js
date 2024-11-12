10d78b7557c1857a1b5a733449e68e767a158203
YSB-RB <yousub.ali@roybrand.com>
2024-11-12 21:49:04 +0530

    initail commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+export { Button } from "./components/Button"
+export { Alert } from "./components/Alert"
+
+
+// export { Footer } from "./components/Footer"
+// export { Sidebar } from "./components/Sidebar"
+// export { Navbar } from "./components/Navabr"
+
+
+
+export { HomePage } from "./pages/home"
+export { AboutPage } from "./pages/about"
+export { ContactPage } from "./pages/contact"
